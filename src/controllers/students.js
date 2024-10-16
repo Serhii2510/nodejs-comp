151bbd30cbfa5623a4ba8c384f85c1fcc3f5c42d
Serhii Ivashchenko <s.ivashchenko.1989@gmail.com>
2024-10-16 10:16:09 +0300

    validation and pagination

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -2,10 +2,24 @@ import createHttpError from 'http-errors';
 
 import { createStudent, deleteStudent, getAllStudents, getStudentById, updateStudent } from "../services/students.js";
 
-export const getStudentsController = async (req, res) => {
-    const students = await getAllStudents();
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
-    res.json({
+export const getStudentsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
+  const students = await getAllStudents({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
+
+  res.json({
     status: 200,
     message: 'Successfully found students!',
     data: students,
