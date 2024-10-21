6d31ac8530ecf988715722fe4e3a238430ded033
Serhii Ivashchenko <s.ivashchenko.1989@gmail.com>
2024-10-21 17:44:29 +0300

    cookies logout and session

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+  registerUserController,
+} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
-
-import { loginUserSchema } from '../validation/auth.js';
-import { loginUserController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -20,5 +22,9 @@ router.post(
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
+
+router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
